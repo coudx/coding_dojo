d8534d34960035c8051839111f4497bc8f1c5b94
cloud xu <cloudxuanxu@gmail.com>
2021-09-06 22:02:33 -0700

    users crud

--- python/flask_mysql/crud/user_cr/server.py
+++ python/flask_mysql/crud/user_cr/server.py
@@ -4,6 +4,11 @@ from users import Users
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return redirect("/users")
+
+
 @app.route("/users")
 def read():
     print(Users.get_all())
@@ -20,6 +25,32 @@ def add():
         "email": request.form["email"],
     }
     Users.add(data)
+    return redirect("/users")
+
+
+@app.route("/users/<id>")
+def view(id):
+    print(Users.get(id))
+    return render_template("view.html", data=Users.get(id)[0])
+
+
+@app.route("/users/<id>/edit", methods=["POST", "GET"])
+def edit(id):
+    if request.method == "GET":
+        return render_template("edit.html", data=Users.get(id)[0])
+    data = {
+        "id": id,
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"],
+    }
+    Users.update(data)
+    return redirect("/users/" + str(id))
+
+
+@app.route("/users/<id>/delete")
+def delete(id):
+    Users.delete(id)
     return redirect("/users")
 
 
