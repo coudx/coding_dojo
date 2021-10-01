add291913c741bd72b4fa913e86af743ce91a256
cloud xu <cloud.xuan.xu@gmail.com>
2021-10-01 13:24:57 -0700

    hello human

--- java/spring/hello_human/src/main/java/com/hello_human/controller/Hello.java
+++ java/spring/hello_human/src/main/java/com/hello_human/controller/Hello.java
@@ -7,13 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
     @RequestMapping("/")
-    public String index(@RequestParam(required = false) String name, @RequestParam(required = false) String lastname) {
+    public String index(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String lastname,
+            @RequestParam(required = false) Integer times
+    ) {
         String line = "Hello ";
         if (name != null){
             line += name + " ";
         }
         if (lastname != null) {
             line += lastname;
+        }
+        if (times != null) {
+            String s = line;
+            for(int i = 1; i<times; i++){
+                line += (" " + s);
+            }
         }
         return line;
     }
